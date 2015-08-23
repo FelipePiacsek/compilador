2ef5243a79db7f315fdd378ec0cb76f39a205389
unknown <felipe.piacsek@gmail.com>
2015-08-23 12:30:19 -0300

    Refactor no teste de consistência da tabela.

--- src/lexico/Aplicacao.java
+++ src/lexico/Aplicacao.java
@@ -15,30 +15,11 @@ public class Aplicacao {
 	public static void main(String[] args) {
 		TabelaSimbolosTerminaisRepository repository = new TabelaSimbolosTerminaisRepository();
 		TabelaSimbolosTerminais tabela = repository.loadTable();
+		TabelaSimbolosTerminaisTeste teste = new TabelaSimbolosTerminaisTeste(tabela);
 //		tabela.print();
-		validateTable(tabela);
+		teste.validateTable();
 	}
 	
-	public static void validateTable(TabelaSimbolosTerminais tabela){
-		TabelaSimbolosTerminaisRepository repository = new TabelaSimbolosTerminaisRepository();
-		List<String> palavras = repository.readFromFile();
-		System.out.println(palavras.size() + " palavras.");
-		List<String> erros = new ArrayList<String>();
-		for (String palavra : palavras) {
-			int i = tabela.insereBusca(palavra, CONSULTA);
-			String s = tabela.getSimbolo(i);
-			if(!s.equals(palavra)){	
-				erros.add(palavra);
-			}
-		}
-		if(erros.isEmpty()){
-			System.out.println("Tabela funcionando corretamente!");
-		}else{
-			System.out.println("Tabela apresentando erro para o(s) seguinte(s) registro(s): ");
-			for (String erro : erros) {
-				System.out.println(erro);
-			}
-		}
-	}
+
 
 }
---
+++ src/lexico/TabelaSimbolosTerminaisTeste.java
@@ -0,0 +1,39 @@
+package lexico;
+
+import static lexico.TabelaSimbolosTerminais.Modo.CONSULTA;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TabelaSimbolosTerminaisTeste{
+
+	private TabelaSimbolosTerminais tabela;
+	
+	private TabelaSimbolosTerminaisRepository repository;
+	
+	public TabelaSimbolosTerminaisTeste(TabelaSimbolosTerminais t) {
+		this.tabela = t;
+		repository = new TabelaSimbolosTerminaisRepository();
+	}
+	
+	public void validateTable(){
+		List<String> palavras = repository.readFromFile();
+		System.out.println(palavras.size() + " palavras.");
+		List<String> erros = new ArrayList<String>();
+		for (String palavra : palavras) {
+			int i = tabela.insereBusca(palavra, CONSULTA);
+			String s = tabela.getSimbolo(i);
+			if(!s.equals(palavra)){	
+				erros.add(palavra);
+			}
+		}
+		if(erros.isEmpty()){
+			System.out.println("Tabela funcionando corretamente!");
+		}else{
+			System.out.println("Tabela apresentando erro para o(s) seguinte(s) registro(s): ");
+			for (String erro : erros) {
+				System.out.println(erro);
+			}
+		}
+	}
+}
